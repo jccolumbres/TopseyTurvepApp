7aaf46655a17174a8bf5197f99a3887fdd14beb5
Jigs <jhegs.knightdev@gmail.com>
2018-10-11 11:50:58 +0800

    Added method for fetching db records

--- app/src/main/java/info/adavis/topsy/turvey/db/TopsyTurveyDataSource.java
+++ app/src/main/java/info/adavis/topsy/turvey/db/TopsyTurveyDataSource.java
@@ -2,9 +2,11 @@ package info.adavis.topsy.turvey.db;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import info.adavis.topsy.turvey.models.Recipe;
@@ -58,5 +60,42 @@ public class TopsyTurveyDataSource {
         long rowId = database.insert(RecipeContract.RecipeStepEntry.TABLE_NAME,null,values);
 
         Log.d(TAG,"Inserted row step id: " + rowId);
+    }
+
+    public List<Recipe> getAllRecipes(){
+
+        // This will store your query results later
+        List<Recipe> recipes = new ArrayList<>();
+
+        //You sql command to fetch records
+        String selectQuery = "SELECT * FROM recipe" ;
+
+        //returns query result
+            //cursor objects start at an index right before your first record
+            //rawQuery also accepts selectionArgs if the sql commands contains a WHERE clause in this case there's none
+        Cursor cursor = database.rawQuery(selectQuery,null);
+        try {
+            //start going through your query results
+            while (cursor.moveToNext()){
+                //Get columns from Contract class
+                Recipe recipe = new Recipe(
+                        cursor.getString(cursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_NAME)),
+                        cursor.getString(cursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_DESCRIPTION)),
+                        cursor.getInt(cursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_IMAGE_RESOURCE_ID)));
+                //don't forget to add the id
+                recipe.setId(cursor.getLong(cursor.getColumnIndex(RecipeContract.RecipeEntry._ID)));
+
+                //add the new object the the List data type from earlier
+                recipes.add(recipe);
+            }
+        }finally {
+            //close the cursor after use
+            if (cursor!=null && !cursor.isClosed())
+            {
+                cursor.close();
+            }
+        }
+        //return list object for use
+        return recipes;
     }
 }
